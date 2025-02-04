02bfeae5782e20b8d61b6a6117e7e2e3741e19ed
VitorLFonseca <vitorlemedf@gmail.com>
2025-02-04 15:25:45 -0300

    Amigo secreto base

---
+++ app.js
@@ -0,0 +1,30 @@
+let amigos = [];
+
+function adicionarAmigo(){
+    let nomes = document.querySelector('input').value;
+    if (nomes == "") {
+        alert('Por favor, insira um nome');
+        return;
+    }
+    amigos.push(nomes);
+    document.querySelector('input').value = "";
+    
+    let lista = document.querySelector('#listaAmigos');
+    lista.innerHTML = "";
+    
+    amigos.forEach(function(amigo) {
+        let li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) { 
+        alert('Não há amigos cadastrados para sortear.');
+        return;
+    }
+    
+    let sorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    document.querySelector('#resultado').innerHTML = 'O amigo sorteado é: ' + sorteado;
+}
